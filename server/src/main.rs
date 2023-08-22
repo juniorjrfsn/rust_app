95da5a6bfc5f4bd4ab877e38887d14f8c15563e3
juniorjrfsn <junior.jrfsn@gmail.com>
2023-08-22 15:19:14 -0400

    ..

--- server/src/main.rs
+++ server/src/main.rs
@@ -6,6 +6,5 @@ fn main() {
     let server = Server::new(|request, mut response| {
         Ok(response.body("Hello, world!".as_bytes().to_vec())?)
     });
-
     server.listen("127.0.0.1", "7979");
 }
