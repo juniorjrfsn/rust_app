fe6f8abb4fc77a34b67c1601e67f677dd8a75692
juniorjrfsn <junior.jrfsn@gmail.com>
2023-08-22 16:03:33 -0400

    ..

--- sqliteexemplo/src/main.rs
+++ sqliteexemplo/src/main.rs
@@ -8,7 +8,7 @@ use crate::conexao::codigos::codes;
 mod functions;
 use crate::functions::janelas::janela_mensagem;
 
-pub(crate) fn main(){
+fn main(){
     // println!("Connection : {:?}",   if _fn1.Ok(10) { _fn1  } else {   Err(10) } );
 
     let _fn1 = conectdatabase::create_database();
