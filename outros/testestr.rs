5778aa1e4b71063ef977d3c940161c51f9c4ede9
juniorjrfsn <junior.jrfsn@gmail.com>
2023-08-23 15:25:05 -0400

    ..

---
+++ outros/testestr.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let strVar : &'static str = "str";
+    let stringVar : String = "String".to_string(); // precisa converter o tipo
+    teste(strVar);
+    teste(&stringVar);
+}
+
+fn teste(texto: &str) {
+    println!("Isto é um {}", texto);
+}
