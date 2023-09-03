945f2af2d715a483bba5b742c32f9dc8d382a295
juniorjrfsn <70738507+juniorjrfsn@users.noreply.github.com>
2023-09-03 17:07:10 -0400

    Update function return types to Result<(), rusqlite::Error> (generated by useblackbox.ai )

--- sqlitelinuxbasic/src/main.rs
+++ sqlitelinuxbasic/src/main.rs
@@ -8,35 +8,35 @@ use crate::conexao::control_person::ctrl_person;
 use crate::conexao::codigos::codes;
 
 fn main() {
-    let _fn1 = conectdatabase::create_database();
+    let _fn1: Result<(), rusqlite::Error> = conectdatabase::create_database();
     println!("-------------------------------------------" );
 
-    let _fn2 = migrationtable::migration_create_table();
+    let _fn2: Result<(), rusqlite::Error> = migrationtable::migration_create_table();
     println!("-------------------------------------------" );
 
     let _fn3 = codes::get_codes();
     println!("-------------------------------------------" );
 
     let c: bool  = true;
-    let _fn3 = ctrl_cat::registrar(c);
+    let _fn3: Result<(), rusqlite::Error> = ctrl_cat::registrar(c);
     println!("-------------------------------------------" );
 
-    let _fn4 = ctrl_cat::get_cats();
+    let _fn4: Result<(), rusqlite::Error> = ctrl_cat::get_cats();
     println!("-------------------------------------------" );
 
     let p  = true;
-    let _fn3 = ctrl_person::registrar(p);
+    let _fn3: Result<(), rusqlite::Error> = ctrl_person::registrar(p);
     println!("-------------------------------------------" );
 
-    let hello = String::from("Hello, world! agora siim").to_owned();
+    let hello: String = String::from("Hello, world! agora siim").to_owned();
     let vec:Vec<u8> = hello.into_bytes();
 
     // let vec: Vec<u8> = vec![0xaa, 0xfc, 0x09, 0x09];
     let op: Option<Vec<u8>> = Some(vec) ;
-    let _fn4 = ctrl_person::update_row(1, "John", op, false);
+    let _fn5 = ctrl_person::update_row(1, "John", op, false);
     println!("-------------------------------------------" );
 
-    let _fn5 = ctrl_person::get_persons();
+    let _fn6: Result<(), rusqlite::Error> = ctrl_person::get_persons();
     println!("-------------------------------------------" );
 
 }
