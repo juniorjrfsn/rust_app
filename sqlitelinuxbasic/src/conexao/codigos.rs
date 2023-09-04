512f14f19d32e873976ac44dc013d3ee2e8e8893
juniorjrfsn <70738507+juniorjrfsn@users.noreply.github.com>
2023-09-04 00:03:40 -0400

    Updated Rust version from 1.70.0 to 1.72.0 (generated by useblackbox.ai )

--- sqlitelinuxbasic/src/conexao/codigos.rs
+++ sqlitelinuxbasic/src/conexao/codigos.rs
@@ -1,114 +1,107 @@
 pub mod codes {
 
-	use std::collections::HashMap;
+    use std::collections::HashMap;
 
-	use std::ops::{Index, IndexMut};
-	pub struct Vector3d<T> {
-	    pub x: T,
-	    pub y: T,
-	    pub z: T,
-	}
-	impl<T> Index<usize> for Vector3d<T> {
-	    type Output = T;
-	    fn index(&self, index: usize) -> &T {
-	        match index {
-	            0 => &self.x,
-	            1 => &self.y,
-	            2 => &self.z,
-	            n => panic!("Invalid Vector3d index: {}", n)
-	        }
-	    }
-	}
-	impl<T> IndexMut<usize> for Vector3d<T> {
-	    fn index_mut(&mut self, index: usize) -> &mut T {
-	        match index {
-	            0 => &mut self.x,
-	            1 => &mut self.y,
-	            2 => &mut self.z,
-	            n => panic!("Invalid Vector3d index: {}", n)
-	        }
-	    }
-	}
+    use std::ops::{Index, IndexMut};
+    pub struct Vector3d<T> {
+        pub x: T,
+        pub y: T,
+        pub z: T,
+    }
+    impl<T> Index<usize> for Vector3d<T> {
+        type Output = T;
+        fn index(&self, index: usize) -> &T {
+            match index {
+                0 => &self.x,
+                1 => &self.y,
+                2 => &self.z,
+                n => panic!("Invalid Vector3d index: {}", n),
+            }
+        }
+    }
+    impl<T> IndexMut<usize> for Vector3d<T> {
+        fn index_mut(&mut self, index: usize) -> &mut T {
+            match index {
+                0 => &mut self.x,
+                1 => &mut self.y,
+                2 => &mut self.z,
+                n => panic!("Invalid Vector3d index: {}", n),
+            }
+        }
+    }
 
-	pub fn get_codes() { 
-		let mut state_codes: HashMap<&str, &str> = HashMap::new();
-		state_codes.insert("NV", "Nevada");
-		state_codes.insert("NY", "New York");
+    pub fn get_codes() {
+        let mut state_codes: HashMap<&str, &str> = HashMap::new();
+        state_codes.insert("NV", "Nevada");
+        state_codes.insert("NY", "New York");
 
-		for (key, val) in state_codes.iter() {
-			println!("key: {key} val: {val}");
-		}
-		println!("................................" );
-		let map = HashMap::from([
-			("a", 1),
-			("b", 2),
-			("c", 3),
-		]);
-		for (key, val) in map.iter() {
-			println!("key: {key} val: {val}");
-		}
-		println!("................................" );
-		let obj = HashMap::from([
-			("key1", "value1"),
-			("key2", "value2")
-		]);
-		for prop in obj.keys() {
-			println!("{}: {}", prop, obj.get(prop).unwrap());
-		} 
-		println!("................................" );
-		let numbers = [1, 2, 3, 4, 5];
-		for number in numbers {
-			println!("{}", number);
-		} 
+        for (key, val) in state_codes.iter() {
+            println!("key: {key} val: {val}");
+        }
+        println!("................................");
+        let map = HashMap::from([("a", 1), ("b", 2), ("c", 3)]);
+        for (key, val) in map.iter() {
+            println!("key: {key} val: {val}");
+        }
+        println!("................................");
+        let obj = HashMap::from([("key1", "value1"), ("key2", "value2")]);
+        for prop in obj.keys() {
+            println!("{}: {}", prop, obj.get(prop).unwrap());
+        }
+        println!("................................");
+        let numbers = [1, 2, 3, 4, 5];
+        for number in numbers {
+            println!("{}", number);
+        }
 
-		println!(".............. find first .................." );
-		let numbers: [i32; 5] = [1, 2,  4, 5, 6];
-		let first_even: Option<&i32> = numbers.iter().find(|x: &&i32| *x % 3 == 0);
-		println!("{:?}", first_even.unwrap());
+        println!(".............. find first ..................");
+        let numbers: [i32; 5] = [1, 2, 4, 5, 6];
+        let first_even: Option<&i32> = numbers.iter().find(|x: &&i32| *x % 3 == 0);
+        println!("{:?}", first_even.unwrap());
 
+        println!("........ filtro de palavras que tenha a letra a .......");
+        let palavras: Vec<&str> = vec![
+            "casa", "carro", "árvore", "cidade", "cão", "gato", "flor", "mar", "lua", "sol",
+        ];
+        let palavras_com_a: Vec<&str> = palavras
+            .iter()
+            .filter(|&&palavra| palavra.contains('a'))
+            .cloned()
+            .collect();
+        println!("{:?}", palavras_com_a);
 
-		println!("........ filtro de palavras que tenha a letra a ......." );
-		let palavras: Vec<&str> = vec!["casa", "carro", "árvore", "cidade", "cão", "gato", "flor", "mar", "lua", "sol"];
-		let palavras_com_a: Vec<&str> = palavras
-			.iter()
-			.filter(|&&palavra| palavra.contains('a'))
-			.cloned::<_>()
-			.collect(); 
-		println!("{:?}", palavras_com_a);
- 
+        println!("................................");
+        let numbers: [String; 3] = [
+            "1 DHFGUTY".to_string(),
+            "2TYUTY".to_string(),
+            "3YT456".to_string(),
+        ];
+        numbers.iter().for_each(|x: &String| println!("{}", x));
 
-		println!("................................" );
-		let numbers: [String; 3] = ["1 DHFGUTY".to_string(), "2TYUTY".to_string(), "3YT456".to_string()];
-		numbers.iter().for_each(|x: &String| println!("{}", x));
-
-
-	 
-
-
-		println!("................................" );
-		let names = ["Sam", "Janet", "Hunter"];
-		let csv = names.join(" - ");
-		println!("{}", csv);
-		println!("................................" );
-		let my_array1 = [1, 2, 3, 4, 5];
-		let mut index = 0; 
-		while index < my_array1.len() {
-			println!("{}", my_array1[index]);
-			index += 1;
-		}
-		println!("................................" );
-		let my_array2 = [1, 2, 3, 4, 5];
-		for item in my_array2.iter() {
-			println!("{}", item);
-		}
-		println!("................................" );
-		let my_array = [1, 2, 3, 4, 5];
-		for (index, item) in my_array.iter().enumerate() {
-			println!("{}: {}", index, item);
-		}
-		println!("................................" );
-		let my_vec: Vec<u8> = vec![72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100]; // "Hello World" in ASCII
-		let vec_to_string = String::from_utf8(my_vec).unwrap(); // Converting to string
-		println!("{}", vec_to_string); // Output: Hello World
-	}
+        println!("................................");
+        let names: [&str; 3] = ["Sam", "Janet", "Hunter"];
+        let csv = names.join(" - ");
+        println!("{}", csv);
+        println!("................................");
+        let my_array1: [i32; 5] = [1, 2, 3, 4, 5];
+        let mut index: usize = 0;
+        while index < my_array1.len() {
+            println!("{}", my_array1[index]);
+            index += 1;
+        }
+        println!("................................");
+        let my_array2 = [1, 2, 3, 4, 5];
+        for item in my_array2.iter() {
+            println!("{}", item);
+        }
+        println!("................................");
+        let my_array: [i32; 5] = [1, 2, 3, 4, 5];
+        for (item, index) in my_array.iter().enumerate() {
+            println!("{}: {}", index, item);
+        }
+        println!("................................");
+        let my_vec: Vec<u8> = vec![72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100]; // "Hello World" in ASCII
+        let vec_to_string = String::from_utf8(my_vec).unwrap(); // Converting to string
+        println!("{}", vec_to_string); // Output: Hello World
+    }
 }
--- sqlitelinuxbasic/src/main.rs
+++ sqlitelinuxbasic/src/main.rs
@@ -1,49 +1,46 @@
-
-
 mod conexao;
+use crate::conexao::codigos::codes;
 use crate::conexao::conedatabase::conectdatabase;
-use crate::conexao::migrationdb::migrationtable;
 use crate::conexao::control_cat::ctrl_cat;
 use crate::conexao::control_person::ctrl_person;
-use crate::conexao::codigos::codes;
+use crate::conexao::migrationdb::migrationtable;
 
 fn main() {
     // Cria o banco de dados
     let _fn1: Result<(), rusqlite::Error> = conectdatabase::create_database();
-    println!("-------------------------------------------" );
-    
+    println!("-------------------------------------------");
+
     // Cria as tabelas
     let _fn2: Result<(), rusqlite::Error> = migrationtable::migration_create_table();
-    println!("-------------------------------------------" );
-    
+    println!("-------------------------------------------");
+
     // Gera os códigos
     let _fn3 = codes::get_codes();
-    println!("-------------------------------------------" );
-    
+    println!("-------------------------------------------");
+
     // Registra um gato
-    let c: bool  = true;
+    let c: bool = true;
     let _fn3: Result<(), rusqlite::Error> = ctrl_cat::registrar(c);
-    println!("-------------------------------------------" );
-    
+    println!("-------------------------------------------");
+
     // Busca todos os gatos
     let _fn4: Result<(), rusqlite::Error> = ctrl_cat::get_cats();
-    println!("-------------------------------------------" );
-    
+    println!("-------------------------------------------");
+
     // Registra uma pessoa
-    let p  = true;
+    let p = true;
     let _fn3: Result<(), rusqlite::Error> = ctrl_person::registrar(p);
-    println!("-------------------------------------------" );
+    println!("-------------------------------------------");
 
     // Atualiza uma pessoa
     let hello: String = String::from("Hello, world! agora siim").to_owned();
-    let vec:Vec<u8> = hello.into_bytes();
+    let vec: Vec<u8> = hello.into_bytes();
     // let vec: Vec<u8> = vec![0xaa, 0xfc, 0x09, 0x09];
-    let op: Option<Vec<u8>> = Some(vec) ;
+    let op: Option<Vec<u8>> = Some(vec);
     let _fn5 = ctrl_person::update_row(1, "John", op, false);
-    println!("-------------------------------------------" );
+    println!("-------------------------------------------");
 
     // Busca todas as pessoas
     let _fn6: Result<(), rusqlite::Error> = ctrl_person::get_persons();
-    println!("-------------------------------------------" );
-
+    println!("-------------------------------------------");
 }
