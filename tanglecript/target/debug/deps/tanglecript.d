501950ab7a086400f460a594d062706b2f6896ee
juniorjrfsn <70738507+juniorjrfsn@users.noreply.github.com>
2024-03-05 16:04:22 -0400

    Update Rust version from 1.70.0 to 1.76.0 (generated by blackbox.ai )

---
+++ tanglecript/target/debug/deps/tanglecript.d
@@ -0,0 +1,5 @@
+D:\projetos\rust_app\tanglecript\target\debug\deps\tanglecript.exe: src\main.rs
+
+D:\projetos\rust_app\tanglecript\target\debug\deps\tanglecript.d: src\main.rs
+
+src\main.rs:
---
+++ tanglecript/target/debug/tanglecript.d
@@ -0,0 +1,1 @@
+D:\projetos\rust_app\tanglecript\target\debug\tanglecript.exe: D:\projetos\rust_app\tanglecript\src\main.rs
