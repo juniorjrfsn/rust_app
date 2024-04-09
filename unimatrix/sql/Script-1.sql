abaf892caff92d9b794c82b9621385677ba6c092
juniorjrfsn <70738507+juniorjrfsn@users.noreply.github.com>
2024-04-09 09:06:21 -0400

    SQL script update: - Delete all records from the 'conta' table and insert new records with updated values. - Add new record to 'conta' table. - Change data type of 'c\_valor' column from NUMERIC to REAL in 'conta' table. - Add new table 'movi' with columns for salary-related data.
    Database content change:
    - New records with salary values and descriptions added to 'conta' table.
    - New 'movi' table created with salary-related data.
    
    Note: The provided diff also includes a binary file change for 'unimatrix.db', which is not covered in this description.

--- unimatrix/sql/Script-1.sql
+++ unimatrix/sql/Script-1.sql
@@ -1,14 +1,25 @@
---  INSERT INTO conta(c_id, c_chave, c_valor, c_dt_insert, c_dt_update, c_dif_saldo_ant, c_descr)
---  VALUES(NULL, 'WXYSwxyz',  9999999981230.936,'HOJE' , datetime(), 0, 'master');
+DELETE FROM conta;
+INSERT INTO conta (c_chave,c_senha,c_valor,c_dt_insert,c_dt_update,c_dif_saldo_ant,c_descr) VALUES
+	 ('WXYSwxyz',	NULL,	99818982597.64264,	'2024-04-09 12:34:45',	'',	0.0,	'master'),
+	 ('7XVFDGHJDF',	NULL,	745676.56,			'2024-04-09 12:34:45',	'',	0.0,	''),
+	 ('2K34IU44',	NULL,	16456.94536,		'2024-04-09 12:34:45',	'',	0.0,	''),
+	 ('BVNB4534',	NULL,	7334.567,			'2024-04-09 12:34:45',	'',	0.0,	''),
+	 ('KLJ4HL23KJ',	NULL,	745676.75,			'2024-04-09 12:34:45',	'',	0.0,	''),
+	 ('H5DF78G6',	NULL,	5645645.936,		'2024-04-09 12:34:45',	'',	0.0,	''),
+	 ('WOIU56H',	NULL,	745676.24,			'2024-04-09 12:34:45',	'',	0.0,	''),
+	 ('SDHDFGH6',	NULL,	134345345.73,		'2024-04-09 12:34:45',	'',	0.0,	''),
+	 ('DFHFG43J',	NULL,	745676.624,			'2024-04-09 12:34:45',	'',	0.0,	''),
+	 ('SDFSD5DH',	NULL,	3456456.245,		'2024-04-09 12:34:45',	'',	0.0,	'');
+INSERT INTO conta (c_chave,c_senha,c_valor,c_dt_insert,c_dt_update,c_dif_saldo_ant,c_descr) VALUES
+	 ('NM7M5VB6N',	NULL,	34563456.76,		'2024-04-09 12:34:45',	'',	0.0,	'');
 
- 
+
 -- UPDATE conta
 -- SET   c_descr=NULL
 -- WHERE c_id>0;
-
+ 
 SELECT c_id, c_chave, c_valor, c_dt_insert, c_dt_update, c_dif_saldo_ant, c_descr
 FROM conta;
-
 
 SELECT SUM(c_valor) AS TOTAL, SUM(c_dif_saldo_ant) AS TOTAL_MOV  FROM conta
  
@@ -21,19 +32,19 @@ CREATE TABLE conta (
 		c_id			INTEGER NOT NULL	PRIMARY	KEY	AUTOINCREMENT
 	, 	c_chave			TEXT	NOT NULL
 	,	c_senha			TEXT
-	,	c_valor			NUMERIC NOT NULL
+	,	c_valor			REAL 	NOT NULL
 	,	c_dt_insert		TEXT 	NOT NULL
 	,	c_dt_update		TEXT
-	,	c_dif_saldo_ant	NUMERIC
+	,	c_dif_saldo_ant	REAL
 	,	c_descr			TEXT
 );
 
 CREATE TABLE movi (
 		c_chave			TEXT	NOT NULL
-	,	m_saldo_ant		NUMERIC NOT NULL
-	,	m_entrada		NUMERIC NOT NULL
-	,	m_saida			NUMERIC NOT NULL
-	,	m_saldo			NUMERIC NOT NULL
+	,	m_saldo_ant		REAL NOT NULL
+	,	m_entrada		REAL NOT NULL
+	,	m_saida			REAL NOT NULL
+	,	m_saldo			REAL NOT NULL
 	,	m_descr			TEXT
 );
 
