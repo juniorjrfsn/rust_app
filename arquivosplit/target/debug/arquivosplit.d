d18f9d7aa3d407877165d4c4bf1dcf62d562124f
juniorjrfsn <70738507+juniorjrfsn@users.noreply.github.com>
2024-05-01 22:34:23 -0400

    ,,

--- arquivosplit/target/debug/arquivosplit.d
+++ arquivosplit/target/debug/arquivosplit.d
@@ -1,1 +1,1 @@
-/home/junior/Documentos/projetos/rustapp/rust_app/arquivosplit/target/debug/arquivosplit: /home/junior/Documentos/projetos/rustapp/rust_app/arquivosplit/src/main.rs
+/home/junior/Documentos/projetos/rust_app/arquivosplit/target/debug/arquivosplit: /home/junior/Documentos/projetos/rust_app/arquivosplit/src/main.rs
