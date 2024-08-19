719d52eb36336f816e995882df2b26fae4a7b8d8
juniorjrfsn <70738507+juniorjrfsn@users.noreply.github.com>
2024-08-19 13:36:42 -0400

    "Updated database path in create_database function"

--- estimulo/src/conexao/conedatabase.rs
+++ estimulo/src/conexao/conedatabase.rs
@@ -2,7 +2,7 @@ pub mod conectdatabase {
 	use rusqlite::{Connection, Result};
 
 	pub fn create_database()  -> Result<()> {
-		let _path = "estimulo.db";
+		let _path = "consciencia/estimulo.db";
 		// let path = "./cats.db";
 		let db = Connection::open(_path)?;
 		// Use the database somehow...
