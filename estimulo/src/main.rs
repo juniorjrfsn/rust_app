cd52fe8f10f20f0379618d6987fcf9f10e8cb318
juniorjrfsn <70738507+juniorjrfsn@users.noreply.github.com>
2024-08-28 19:56:18 -0400

    ...

--- estimulo/src/main.rs
+++ estimulo/src/main.rs
@@ -8,12 +8,30 @@ use std::os::unix;
 use std::os::windows;
 use std::path::Path;
 
+
+mod exemplos;
+use crate::exemplos::codigos::codes;
+
 mod conexao;
 use crate::conexao::conedatabase::conectdatabase;
+
 fn main() {
     println!("Hello, world!");
     let _fn1 = conectdatabase::create_database();
     println!("-------------------------------------------" );
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Falha ao ler a linha");
+    // let num: i32 = match input.trim().parse() { Ok(num) => num, Err(_) => 0, };
+    
+
+    match (match input.trim().parse() { Ok(num) => num, Err(_) => 0, }) == 0 {
+        true => {
+            let _fn1: () = codes::get_codes_string(Some(false));
+        }
+        false => {
+            let _fn1: () = codes::get_codes_string(Some(true));
+        }
+    }
 }
 
 
