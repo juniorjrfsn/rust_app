c42c7f2c5c9975d62e83d9c29e66d2ec05b26146
juniorjrfsn <70738507+juniorjrfsn@users.noreply.github.com>
2025-01-27 21:58:26 -0400

    `Update .rustc_info.json with new rustc fingerprint and outputs`
    `Update .rustc_info.json with new rustc fingerprint and outputs`

---
+++ conhecimento/target/debug/deps/conhecimento-d97995db40a13264.d
@@ -0,0 +1,9 @@
+/home/junior/Documentos/projetos/rust_app/conhecimento/target/debug/deps/libconhecimento-d97995db40a13264.rmeta: src/main.rs Cargo.toml
+
+/home/junior/Documentos/projetos/rust_app/conhecimento/target/debug/deps/conhecimento-d97995db40a13264.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
